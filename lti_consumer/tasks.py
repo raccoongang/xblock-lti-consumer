62b9f54d9d47595796c5a6f1fcffec0bc3e5bbfa
Andrey Lykhoman <andrey.likhoman@gmail.com>
2020-07-14 17:31:04 +0300

    BV-629: Fix style mistake.

--- lti_consumer/tasks.py
+++ lti_consumer/tasks.py
@@ -3,6 +3,7 @@ Defines asynchronous celery task for sending email notification (via EmailMultiA
 pertaining if a user got a grade in the LTI window, an email message sends for him
 """
 from celery.decorators import task  # pylint: disable=import-error
+
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from xblockutils.resources import ResourceLoader
